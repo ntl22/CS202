3d0024cac2220e45a03f09d326c6cb8f47c1b9d6
TruongTho3345 <100506191+TruongTho3345@users.noreply.github.com>
2022-12-27 17:30:05 +0700

    Adding State

--- src/Framework/State/state.hpp
+++ src/Framework/State/state.hpp
@@ -2,4 +2,20 @@
 #define SRC_GAME_ENTITY_STATE
 // TODO: add a state (screen) base class
 
+class State
+{
+public:
+    typedef std::unique_ptr<State> pointer_type;
+
+    virtual ~State() = default;
+
+    /**
+     * Prerequisite task of a state
+     */
+
+    virtual void handleEvent(const sf::Event& ev) = 0;
+    virtual void update(sf::Time dt) = 0;
+    virtual void draw() = 0;
+};
+
 #endif /* SRC_GAME_ENTITY_STATE */
