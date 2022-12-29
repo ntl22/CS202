b7f171daf3c6109d29d2bd98aa3ff6d1105f3012
ntl22 <nxtung16@outlook.com>
2022-12-29 12:24:47 +0700

    add entity base class

---
+++ src/Game/Entity/entity.hpp
@@ -0,0 +1,15 @@
+#ifndef SRC_GAME_ENTITY_OBJECT
+#define SRC_GAME_ENTITY_OBJECT
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+
+struct Entity
+{
+    virtual void handleEvent(const sf::Event &ev) = 0;
+    virtual void update(sf::Time dt) = 0;
+    virtual void draw(sf::RenderWindow &window) = 0;
+
+    virtual void playLocalSound() {}
+};
+
+#endif /* SRC_GAME_ENTITY_OBJECT */
