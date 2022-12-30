6a38ac361f3c870380edadfeb843e858ba76f2d9
TruongTho3345 <100506191+TruongTho3345@users.noreply.github.com>
2022-12-30 17:26:30 +0700

    Adding GameOver screen

---
+++ src/Game/State/finish.cpp
@@ -0,0 +1,124 @@
+#include "finish.hpp"
+
+#include "playing.hpp"
+#include "menu.hpp"
+
+#include <iostream>
+
+FinishState::FinishState(Context& context)
+    : m_context(context), title_font(context.fonts->get(FONTS::Sansation))
+{
+    sf::Vector2f size(m_context.window->getSize());
+
+    title.setOutlineColor(sf::Color::Black);
+
+    title.setString("Game Over");
+    title.setOutlineThickness(5);
+    title.setFont(title_font);
+    title.setCharacterSize(90U);
+    setCenterOrigin(title, title.getLocalBounds());
+    title.setPosition(m_context.window->getView().getCenter() - sf::Vector2f(0.f, 250.f));
+
+    int i;
+
+    for (i = 0; i < 3; i++)
+        button[i] = std::make_unique<Button>(*m_context.window, m_context.fonts->get(FONTS::visitor1));
+
+    button[0]->setText("PLAY AGAIN");
+    button[0]->setHoverColor(sf::Color(255, 219, 62));
+    button[0]->setPosition(m_context.window->getView().getCenter() - sf::Vector2f(0, 100.f));
+    button[0]->setCallback([this]()
+        { m_context.states->push(std::make_unique<PlayingState>(m_context)); });
+
+    button[1]->setText("BACK TO MENU");
+    button[1]->setHoverColor(sf::Color(111, 225, 62));
+    button[1]->setPosition(m_context.window->getView().getCenter());
+    button[1]->setCallback([this]()
+        { m_context.states->push(std::make_unique<MenuState>(m_context), true); });
+
+    button[2]->setText("EXIT");
+    button[2]->setHoverColor(sf::Color(226, 16, 16));
+    button[2]->setPosition(m_context.window->getView().getCenter() + sf::Vector2f(0, 100.f));
+    button[2]->setCallback([this]()
+        { m_context.states->pop(); });
+
+    m_context.musics->setLoop(true);
+    m_context.musics->play(MUSICS::intro);
+
+    background.setTexture(m_context.textures->get(TEXTURES::welcome_bg));
+
+    background.scale(
+        size.x / background.getLocalBounds().width,
+        size.y / background.getLocalBounds().height
+    );
+}
+
+FinishState::~FinishState() {}
+
+void FinishState::handleEvent(const sf::Event& ev)
+{
+    sf::Vector2f pos = sf::Vector2f(sf::Mouse::getPosition(*m_context.window));
+    int i;
+
+    if (ev.type == sf::Event::MouseMoved)
+    {
+        for (i = 0; i < 3; i++)
+        {
+            if (button[i]->isInWidget(pos))
+            {
+                cur = i;
+                break;
+            }
+        }
+    }
+    else if (ev.type == sf::Event::MouseButtonPressed &&
+        ev.mouseButton.button == sf::Mouse::Left &&
+        cur != -1 &&
+        button[cur]->isInWidget(pos))
+    {
+        button[cur]->activate();
+    }
+    else if (ev.type == sf::Event::KeyPressed)
+    {
+        switch (ev.key.code)
+        {
+        case sf::Keyboard::W:
+        case sf::Keyboard::Up:
+            cur = (cur <= 0) ? 2 : (cur - 1);
+            break;
+        case sf::Keyboard::S:
+        case sf::Keyboard::Down:
+            cur = (cur + 1) % 3;
+            break;
+        case sf::Keyboard::Enter:
+            if (cur != -1)
+                button[cur]->activate();
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void FinishState::update(sf::Time dt)
+{
+    int i;
+
+    for (i = 0; i < 3; i++)
+    {
+        if (i == cur)
+            button[i]->focus();
+        else
+            button[i]->unfocus();
+    }
+}
+
+void FinishState::draw()
+{
+    m_context.window->draw(background);
+    m_context.window->draw(title);
+
+    int i;
+    for (i = 0; i < 3; i++)
+        m_context.window->draw(*button[i]);
+}
---
+++ src/Game/State/finish.hpp
@@ -0,0 +1,25 @@
+#ifndef SRC_GAME_STATE_FINISH
+#define SRC_GAME_STATE_FINISH
+#include "../../Framework/pch.hpp"
+
+class FinishState : public State {
+public:
+	FinishState(Context& context);
+
+	~FinishState();
+	    void handleEvent(const sf::Event &ev) override;
+    void update(sf::Time dt) override;
+    void draw() override;
+
+private:
+    sf::Font title_font;
+    sf::Text title;
+    sf::Sprite background;
+
+    std::unique_ptr<Button> button[3];
+    int cur = -1;
+
+    Context& m_context;
+};
+
+#endif /* SRC_GAME_STATE_FINISH */
