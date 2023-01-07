472835c3e4322efca6ebdbc963d13acb6faca2a9
phungptn <ngothaiphung412@gmail.com>
2023-01-07 23:02:17 +0700

    update header function in vehicle.hpp

--- src/Game/Entity/vehicle.hpp
+++ src/Game/Entity/vehicle.hpp
@@ -6,16 +6,22 @@
 
 class Vehicle : public Obstacle
 {
-
+public:
+    void update(sf::Time dt,
+        unsigned velocity,
+        People& people,
+        TrafficLight& light,
+        sf::RenderWindow& window) override;
 };
 
 class Truck : public Vehicle
 {
-
+public:
+    Truck(TextureMap& map);
 };
 
 class Car : public Vehicle
 {
-
+    Car(TextureMap& map);
 };
 #endif /* SRC_GAME_ENTITY_VEHICLE */
