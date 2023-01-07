d4d07be273107fd420a1ecddb333837331e9b2f0
phungptn <ngothaiphung412@gmail.com>
2023-01-07 22:21:36 +0700

    create class Animal and its subclasses

--- src/Game/Entity/animal.hpp
+++ src/Game/Entity/animal.hpp
@@ -1,7 +1,23 @@
 #ifndef SRC_GAME_ENTITY_ANIMAL
 #define SRC_GAME_ENTITY_ANIMAL
+#include "../../Framework/pch.hpp"
 #include "obstacle.hpp"
 
 // TODO: add animal child classes
+
+class Animal : public Obstacle
+{
+};
+
+class Cat : public Animal
+{
+
+};
+
+class Chicken : public Animal
+{
+
+};
+
 
 #endif /* SRC_GAME_ENTITY_ANIMAL */
