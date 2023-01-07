4161b36d674ab88ec96beab81a8f7dace35605b4
phungptn <ngothaiphung412@gmail.com>
2023-01-07 23:03:23 +0700

    implement functions of truck and car in vehicle.cpp

--- src/Game/Entity/vehicle.cpp
+++ src/Game/Entity/vehicle.cpp
@@ -1,1 +1,15 @@
 #include "vehicle.hpp"
+
+Truck::Truck(TextureMap& map)
+{
+    texture = map.get(TEXTURES::truck);
+    sprite.setTexture(texture);
+    sprite.scale(1.5, 1.5);
+}
+
+Car::Car(TextureMap& map)
+{
+    texture = map.get(TEXTURES::car);
+    sprite.setTexture(texture);
+    sprite.scale(1.5, 1.5);
+}
