c2152a5797abd68f073d928dabdd66f034952d95
phungptn <ngothaiphung412@gmail.com>
2023-01-07 22:37:37 +0700

    implement functions in cat and chicken in animal.cpp

--- src/Game/Entity/animal.cpp
+++ src/Game/Entity/animal.cpp
@@ -1,1 +1,16 @@
 #include "animal.hpp"
+
+Cat::Cat(TextureMap& map)
+{
+    texture = map.get(TEXTURES::cat1);
+    sprite.setTexture(texture);
+    sprite.scale(1.5, 1.5);
+}
+
+Chicken::Chicken(TextureMap& map)
+{
+    texture = map.get(TEXTURES::chicken);
+    sprite.setTexture(texture);
+    sprite.scale(1.5, 1.5);
+}
+
