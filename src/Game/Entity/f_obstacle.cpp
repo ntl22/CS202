49c90ee241e4ab0fa52c7a9b7f85ed55e6f30ffa
Nguyen Phuc Bao Uyen <hollyuyensan@gmail.com>
2023-01-08 11:10:14 +0700

    Obstacle list

--- src/Game/Entity/f_obstacle.cpp
+++ src/Game/Entity/f_obstacle.cpp
@@ -1,1 +1,77 @@
 #include "f_obstacle.hpp"
+
+ListOfObstacle::ListOfObstacle(float position, OBJECT_TYPE type, TextureMap &map)
+    : m_type(type)
+{
+    if (m_type == OBJECT_TYPE::NONE)
+        return;
+    spawnObstable(position, map);
+}
+
+void ListOfObstacle::spawnObstable(float position, TextureMap &map)
+{
+    size_t i = 0;
+    int tmp = 1800;
+    int dis;
+    int padding = (m_type == OBJECT_TYPE::CAT || m_type == OBJECT_TYPE::CHICKEN) ? 50 : 0;
+
+    for (i = 0; i < list.size(); i++)
+    {
+        dis = rand() % 201 + 200;
+
+        list[i] = std::move(ObstacleFactory::create(m_type, map));
+        list[i]->setPos(tmp, (int)position);
+        tmp -= (int)(dis + 0.5 * list[i]->getBound().height + padding);
+    }
+}
+
+void ListOfObstacle::draw(sf::RenderWindow &window)
+{
+    if (m_type == OBJECT_TYPE::NONE)
+        return;
+    for (auto &object : list)
+        object->draw(window);
+}
+
+void ListOfObstacle::update(sf::Time dt, unsigned velocity, People &people,
+                            TrafficLight &light, sf::RenderWindow &window)
+{
+    if (m_type == OBJECT_TYPE::NONE)
+        return;
+
+    size_t i, size = list.size();
+    int o_width;
+
+    unsigned w_width = window.getSize().x;
+
+    for (i = 0; i < size; i++)
+    {
+        list[i]->update(dt, velocity, people, light, window);
+
+        list[i]->collisionProcedure(people);
+
+        if (people.getPlayingStatus() == STATUS::DEAD)
+            break;
+
+        if (list[i]->getBound().left > w_width)
+        {
+            size_t i_next = (i == 0 ? size - 1 : i - 1);
+            sf::Vector2i next_pos(list[i_next]->getPos());
+            sf::Vector2i cur_pos(list[i]->getPos());
+            o_width = (int)list[i]->getBound().width;
+
+            int dis = rand() % 201 + 200;
+
+            if (list[i_next]->getPos().x - dis < 0)
+                list[i]->setPos(next_pos.x - dis - o_width, cur_pos.y);
+            else
+                list[i]->setPos(-o_width, cur_pos.y);
+        }
+    }
+}
+
+void ListOfObstacle::handleEvent(const sf::Event &ev)
+{
+    if (m_type == OBJECT_TYPE::NONE)
+        return;
+}
