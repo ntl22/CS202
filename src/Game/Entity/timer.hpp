18d6179c731569348e439d7cbbb0c28b16dfd18d
phungptn <ngothaiphung412@gmail.com>
2023-01-08 11:05:47 +0700

    create timer's headers

--- src/Game/Entity/timer.hpp
+++ src/Game/Entity/timer.hpp
@@ -1,5 +1,29 @@
 #ifndef SRC_GAME_ENTITY_TIMER
 #define SRC_GAME_ENTITY_TIMER
+#include "../../Framework/pch.hpp"
+
 // TODO: Reimplement Timer (Phung)
+
+class Timer
+{
+public:
+    Timer(Context& context);
+
+    sf::Time getTime();
+
+    sf::Time update();
+    void exitPauseState();
+    void draw(sf::RenderWindow& window);
+
+private:
+    void loadGame(std::ifstream& fin);
+    void saveGame(std::ofstream& fout);
+
+
+    sf::Text to_text;
+    sf::Time timer;
+    sf::Clock clock;
+
+};
 
 #endif /* SRC_GAME_ENTITY_TIMER */
